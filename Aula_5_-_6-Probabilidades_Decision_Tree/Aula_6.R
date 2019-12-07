a2a89491726d8970884c659fe95de2e40ed49ac1
Leandro <leandromoraesmendes@gmail.com>
2019-12-07 12:01:40 -0300

    Update_Aula_6
    Arvore de decisão

--- Aula_5_-_6-Probabilidades_Decision_Tree/Aula_6.R
+++ Aula_5_-_6-Probabilidades_Decision_Tree/Aula_6.R
@@ -92,3 +92,27 @@ levels(cat$v2)<-c(levels(cat$v2), "4")
 levels((cat$v2))
 rbind(cat,c(7,4))
 
+
+
+# AD Titanic Sex + Age 
+# 0. Libs
+library(party)
+# 1. Carga de Dados
+d <- read.csv("train.csv")
+# 2. Tratamento de Variaveis
+d$Pclass_c <- as.factor(d$Pclass)
+#d$Fare_c <- as.factor(d$Fare)
+# 3. Separação Treino e Teste
+set.seed(33)
+va<-sample(891)
+treino<-d[va[1:600],]
+teste<-d[va[601:891],]
+# 4. Modelagem
+mod <- ctree(Survived~Sex+Pclass_c, data=treino)
+plot(mod, type="simple")
+p <- predict(mod, newdata=teste)
+# 5. Analise de resultados
+prev <- ifelse(p<.5,0,1)
+# Matriz de Confusão
+table(prev, teste$Survived)
+
